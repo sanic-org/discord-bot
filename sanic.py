f0cc38f5053403fe6deea7001d7603780c0efb64
sunset <aidanstewart@sunsetdeveloper.com>
2021-04-29 14:03:45 -0400

    github issue listener + improved lookup.

--- sanic.py
+++ sanic.py
@@ -1,12 +1,29 @@
+from discord import Message
 from discord.ext import commands
 
 from sanicbot.core.cogs import HelpCog, GitCog
 from sanicbot.core.config import config
 
 bot = commands.Bot(help_command=None, command_prefix=None)
+git_cog = GitCog(bot)
+
+
+@bot.event
+async def on_message(message):
+    await github_issue_listener(message)
+
+
+async def github_issue_listener(message: Message):
+    if not message.author.bot:
+        match = git_cog.issue_pattern.search(message.content)
+        if match:
+            await git_cog.lookup(message.channel, match.group("issue_id"), 'sanic')
+        else:
+            await bot.process_commands(message)
+
 
 if __name__ == '__main__':
     bot.command_prefix = '!'
-    bot.add_cog(GitCog(bot))
+    bot.add_cog(git_cog)
     bot.add_cog(HelpCog(bot))
     bot.run(config['SANIC']['token'], bot=True)
--- sanicbot/core/cogs.py
+++ sanicbot/core/cogs.py
@@ -1,3 +1,5 @@
+import re
+
 import httpx
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -6,32 +8,35 @@ from sanicbot.core.utils import failure_message, success_message
 
 
 class GitCog(commands.Cog):
+    issue_pattern = re.compile(r"#(?P<issue_id>[1,2]\d{3})")
 
-    async def lookup(self, number: int, repo: str):
+    async def lookup(self, ctx: Context, number: int, repo: str):
         """
         Lookup the issue and check if it exists within a repo found in the Sanic organization. Redirection to
         retrieve pull request is automatic due to the fact that Github issues them sequentially.
 
-        :param number: Issue number
+        :param ctx: Discord context.
+
+        :param number: Issue number.
 
         :param repo: Repository of issue being looked up.
 
-        :return: url, response_code.
+        :return: url, response_code
         """
         url = f'https://github.com/sanic-org/{repo}/issues/{number}'
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
+            if response.status_code == 200:
+                await success_message(ctx, f'Issue in {repo} has been found.\n{url}')
+            else:
+                await failure_message(ctx, f'Issue in {repo} has not been found.')
             return str(response.url), response.status_code
 
     @commands.command(aliases=['git', 'gh'])
-    async def retrieve_issue(self, ctx: Context, number: int, repo: str = 'sanic'):
+    async def retrieve_github_issue(self, ctx: Context, number: int, repo: str = 'sanic'):
         if not repo.startswith("sanic"):
             repo = f"sanic-{repo}"
-        url, lookup_status = await self.lookup(number, repo)
-        if lookup_status == 200:
-            await success_message(ctx, f'Issue in {repo} found.\n{url}')
-        else:
-            await failure_message(ctx, f'Issue in {repo} has not been found.')
+        await self.lookup(ctx, number, repo)
 
 
 class HelpCog(commands.Cog):
