3c8ef225268caa120b8b5e9f182ca7fa7cb59bc6
Rob <asyte.io.dev@gmail.com>
2022-09-01 07:31:33 -0400

    added SanicBot wrapper class, REGISTERED_COGS

--- sanic.py
+++ sanic.py
@@ -1,14 +1,71 @@
+import discord
+import aiohttp
+
 from discord.ext import commands
 
-from sanicbot.core.cogs import HelpCog, GitCog
+from sanicbot.core import cogs
+from sanicbot.core import exceptions
 from sanicbot.core.config import config
+from sanicbot.core.startup import startup
 
-bot = commands.Bot(help_command=None, command_prefix=None)
-git_cog = GitCog(bot)
+# Register cogs
+REGISTERED_COGS = [
+    cogs.HelpCog,
+    cogs.GitCog
+]
 
 
+class SanicBot(commands.Bot):
+    DEBUG = config['SANIC']['debug']
+
+    def __init__(self, *args, **kwargs):
+        intents = discord.Intents.default()
+        intents.members = True
+        super().__init__(*args, command_prefix='!', intents=intents, **kwargs)
+
+        self.httpclient = aiohttp.ClientSession()
+
+        for cog in REGISTERED_COGS:
+            self.add_cog(cog(self))
+
+    # Server Events
+    async def on_ready(self):
+        # Set the guild (limit the bot usage to a given guild_id)
+        self.guild = None
+        for guild in self.guilds:
+            if guild.id == config['SANIC']['guild_id']
+                self.guild = guild
+                break
+        if not self.guild:
+            raise exceptions.InvalidGuild()
+
+        # Initialize the server
+        startup.setup_server(self, config)
+
+    async def on_message(self, message):
+        await self.process_commands(message)
+
+    async def close(self):
+        await self.httpclient.close()
+        await super().close()
+
+    async def on_member_join(self, member):
+        pass
+
+    # Helpers
+    async def logit(self, message):
+        if self.DEBUG:
+            await self.debug_channel.send(message)
+        print(message)
+
+
+# Initialize the bot
+bot = SanicBot(help_command=None)
+
+
+# Run the bot
 if __name__ == '__main__':
-    bot.command_prefix = '!'
-    bot.add_cog(git_cog)
-    bot.add_cog(HelpCog(bot))
-    bot.run(config['SANIC']['token'])
+    try:
+        bot.run(config['SANIC']['token'])
+    except KeyboardInterrupt:    
+        pass
