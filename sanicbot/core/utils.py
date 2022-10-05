76771164ac77642e3737ef98388f4e6e58621f34
Rob <asyte.io.dev@gmail.com>
2022-10-04 23:58:22 -0400

    failure/success messages accept channel instead of ctx

--- sanicbot/core/utils.py
+++ sanicbot/core/utils.py
@@ -1,11 +1,10 @@
-from discord import Embed
-from discord.ext.commands import Context
+import nextcord
 
 
-async def failure_message(ctx: Context, message: str):
-    await ctx.send(f":x: **{message}**")
+async def failure_message(channel: nextcord.TextChannel, message: str):
+    await channel.send(f":x: **{message}**")
 
 
-async def success_message(ctx: Context, message: str, embed: Embed = None):
+async def success_message(channel: nextcord.TextChannel, message: str, embed: nextcord.Embed = None):
     message = f":white_check_mark: **{message}**"
-    await ctx.send(message, embed=embed)
+    await channel.send(message, embed=embed)
